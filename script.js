e9978fe46d9dca722252c374577610323d49a7d3
Arc-Data <arclight04@gmail.com>
2022-11-26 10:34:00 +0800

    Adds gridlines functionality

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const sketchBoard = document.querySelector('.sketch-board');
 const gridContainer = document.createElement('div');
 const reset = document.querySelector('#reset');
+const gridButton = document.querySelector('#gridlines');
 gridContainer.classList.add('grid-container');
 
 
 sketchBoard.appendChild(gridContainer)
+
+function gridLines(e) {
+    const divs = document.querySelectorAll('.grid-container > div');
+    if(e.target.textContent == 'Remove Gridlines') {
+        e.target.textContent = 'Add Gridlines';
+        divs.forEach(div => div.classList.remove('grid-item'));
+    } else {
+        e.target.textContent = 'Remove Gridlines';
+        divs.forEach(div => div.classList.add('grid-item'));
+    }
+}
 
 function mouseHover(e) {
     console.log(e.target)
@@ -23,7 +35,6 @@ function initializeGrid(gridItems = 16) {
     for(let i = 0; i < gridItems**2; i++) {
         let gridItem = document.createElement('div');
         gridContainer.appendChild(gridItem);
-        gridItem.classList.add('grid-item')
         gridItem.addEventListener('mousedown', (e) => {
             if(e.button == 0) {
                 e.target.style.background = 'rgb(51,51,51)';
@@ -35,6 +46,7 @@ function initializeGrid(gridItems = 16) {
 }
 
 function resetGrid() {
+    gridButton.textContent = 'Add Gridlines';
     while(gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.lastChild);
     }
@@ -43,5 +55,6 @@ function resetGrid() {
 }
 
 reset.addEventListener('click', resetGrid);
+gridButton.addEventListener('click', gridLines);
 
 initializeGrid();
